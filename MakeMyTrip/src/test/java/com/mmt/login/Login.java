9ec6d07db13a18f0b0fa9c858d7bfe09955a00d1
SeleniumAutomationGitHub <seleniumautomaton@gmail.com>
2016-03-04 20:03:05 +0530

    initial check in.

---
+++ MakeMyTrip/src/test/java/com/mmt/login/Login.java
@@ -0,0 +1,27 @@
+package com.mmt.login;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+public class Login {
+	WebDriver driver;
+	
+	
+  @Test
+  public void test() throws Exception {
+	  Thread.sleep(1000);
+	  driver.get("http://www.spicejet.com/");
+	  System.out.println(driver.getTitle());
+  }
+  
+  
+  
+  @BeforeTest
+  public void beforeTest() {
+	  driver = new FirefoxDriver();
+	  driver.manage().window().maximize();
+  }
+
+}
